8a2203889331166940d7305df06d833fa396f969
Artem <memskov@gmail.com>
2023-05-28 13:36:34 +0200

    .env file added

--- app/db/questions.py
+++ app/db/questions.py
@@ -1,5 +1,7 @@
 import logging
+import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -8,9 +10,17 @@ from app.models.questions import Question
 
 logger = logging.getLogger(__name__)
 
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
 
 def open_session():
-    engine = create_engine('postgresql://postgres:postgres@db/quiz_service_db')
+    engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_host}/{db_name}')
     Session = sessionmaker(bind=engine)
     session = Session()
     logger.info('Успешное подключение к БД')
